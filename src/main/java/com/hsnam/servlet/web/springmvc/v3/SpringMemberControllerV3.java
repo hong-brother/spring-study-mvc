c46fb5205ba6a7330389e79069b29c9b60af7f02
hong-brother <robbins.hs@gmail.com>
2022-04-02 00:43:16 +0900

    스프링 MVC - 구조 이해 추가

---
+++ src/main/java/com/hsnam/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,46 @@
+package com.hsnam.servlet.web.springmvc.v3;
+
+import com.hsnam.servlet.domain.member.Member;
+import com.hsnam.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    public String newFrom() {
+        return "new-form";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String members(Model mv) {
+        List<Member> members = memberRepository.findAll();
+        mv.addAttribute("members", members);
+        return "members";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model
+    ) {
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+}
